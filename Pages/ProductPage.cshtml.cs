df814f1a5877f9559639180f95217ee81cb5ac39
Nekrol <igorbondarev0508@gmail.com>
2023-10-21 13:16:23 +0300

    Добавлена страница товара

--- Pages/ProductPage.cshtml.cs
+++ Pages/ProductPage.cshtml.cs
@@ -1,12 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProductSite.DAL;
+using ProductSite.Models;
 
 namespace ProductSite.Pages
 {
     public class ProductPageModel : PageModel
     {
-        public void OnGet()
+        public Product product { get; set; }
+
+        public readonly ProductDAL _productDAL;
+        public ProductPageModel()
         {
+            _productDAL = new ProductDAL();
+        }
+        public void OnPost(int id)
+        {         
+            product = _productDAL.ChoiceProduct(id);
+        }
+        public void OnGet(int id)
+        {
+            product = _productDAL.ChoiceProduct(id);
         }
     }
 }
