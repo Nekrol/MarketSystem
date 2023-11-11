668622bf8f337a1ce7c37af42007ac180fb7effb
Nekrol <igorbondarev0508@gmail.com>
2023-11-11 13:57:48 +0300

    Рейтинг синхронизирован с базой данных

--- Pages/ProductPage.cshtml.cs
+++ Pages/ProductPage.cshtml.cs
@@ -9,7 +9,8 @@ namespace ProductSite.Pages
     {
         public Product product { get; set; }
         public int rate { get; set; }
-        public int[] stars = new int[5] {1, 2, 3, 4, 5};
+        public int[] stars = new int[5] {5, 4, 3, 2, 1};
+        [BindProperty]
         public int Star { get; set; } = 0;
 
         public readonly ProductDAL _productDAL;
@@ -20,8 +21,13 @@ namespace ProductSite.Pages
         public IActionResult OnPost(int id)
         {         
             product = _productDAL.ChoiceProduct(id);
-            rate = Star;
+            if(Star!=0)
+            {
+                _productDAL.RateProduct(Star, id);             
+            }
+            rate = Utils.RoundUpRating(_productDAL.GetAverageRating(id));
             return Page();
+            
         }
         public void OnGet(int id)
         {
